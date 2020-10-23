1d201b6763c9ecea59cfe0f2a5dbf738bb27d5bc
astriskit <harry2office@gmail.com>
2020-10-23 12:35:15 +0530

    update release

---
+++ docs/precache-manifest.ae85429ef5075114b11bfebf48422de5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6bd78e2c0bb5bda622436b44c3f141",
+    "url": "/astriskit/awesome-github/index.html"
+  },
+  {
+    "revision": "b46f3e038c873c14ffe6",
+    "url": "/astriskit/awesome-github/static/css/main.0093c084.chunk.css"
+  },
+  {
+    "revision": "e6ad6cd455f3954e675a",
+    "url": "/astriskit/awesome-github/static/js/2.996d7d10.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/astriskit/awesome-github/static/js/2.996d7d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46f3e038c873c14ffe6",
+    "url": "/astriskit/awesome-github/static/js/main.5c124985.chunk.js"
+  },
+  {
+    "revision": "6f49405c54b9a2f32b43",
+    "url": "/astriskit/awesome-github/static/js/runtime-main.e8610558.js"
+  },
+  {
+    "revision": "5134db46a99bc43d49c30d5830c988d5",
+    "url": "/astriskit/awesome-github/static/media/spinner.5134db46.gif"
+  }
+]);
