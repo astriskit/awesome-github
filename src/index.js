83a5e250cdba4252378f6428d082ba93422c4ae5
astriskit <harry2office@gmail.com>
2020-10-23 12:31:07 +0530

    s-lining build

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL || "/"}>
+    <BrowserRouter basename={process.env.PUBLIC_URL || "/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
