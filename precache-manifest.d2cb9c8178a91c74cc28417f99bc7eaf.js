bc09f22abc1e3d2ad2b30426a7cc9548cb91c154
astriskit <harry2office@gmail.com>
2020-10-23 13:17:37 +0530

    added site release

---
+++ precache-manifest.d2cb9c8178a91c74cc28417f99bc7eaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297537553abc664ca81a6f08b5f4d957",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccd8cd75b1705d6060f5",
+    "url": "/static/css/main.0093c084.chunk.css"
+  },
+  {
+    "revision": "e6ad6cd455f3954e675a",
+    "url": "/static/js/2.996d7d10.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.996d7d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd8cd75b1705d6060f5",
+    "url": "/static/js/main.19cd453e.chunk.js"
+  },
+  {
+    "revision": "eb5531c6ebaecf29f884",
+    "url": "/static/js/runtime-main.898e7816.js"
+  },
+  {
+    "revision": "5134db46a99bc43d49c30d5830c988d5",
+    "url": "/static/media/spinner.5134db46.gif"
+  }
+]);
