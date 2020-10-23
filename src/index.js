a1cfc447fe1aa46d873275e23da9f8128fb348a9
astriskit <harry2office@gmail.com>
2020-10-23 13:12:25 +0530

    added env

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL || "/"}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
